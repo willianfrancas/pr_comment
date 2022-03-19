caec51f8d7110e4ad193ff31968a118c94f9bf98
willianfrancas <willianfrancas@gmail.com>
2022-03-19 19:18:33 -0300

    adding logic

--- index.js
+++ index.js
@@ -1,18 +1,79 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const pr_number = core.getInput('pr_number', { required: true });
+    const token = core.getInput('token', { required: true });
 
-    core.setOutput('time', new Date().toTimeString());
+    const octokit = new github.getOctokit(token);
+
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: pr_number,
+    });
+
+    let diffData = {
+      additions: 0,
+      deletions: 0,
+      changes: 0
+    };
+
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions;
+      acc.deletions += file.deletions;
+      acc.changes += file.changes;
+      return acc;
+    }, diffData);
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+      Pull request #${pr_number} has be updated with: \n
+        - ${diffData.changes} changes \n
+        - ${diffData.additions} additions \n
+        - ${diffData.deletions} deletions
+      `
+    });
+
+    for (const file of changedFiles) {
+
+      const fileExtension = file.filename.split('.').pop();
+      let label = '';
+      switch (fileExtension) {
+        case 'md':
+          label = 'markdown';
+          break;
+        case 'js':
+          label = 'javascript';
+          break;
+        case 'yml':
+          label = 'yaml';
+          break;
+        case 'yaml':
+          label = 'yaml';
+          break;
+
+        default:
+          label = 'file with no extension';
+          break;
+      }
+      await octokit.rest.issues.addLabels({
+        owner,
+        repo,
+        issue_number: pr_number,
+        labels: [label]
+      });
+    }
+
   } catch (error) {
     core.setFailed(error.message);
   }
